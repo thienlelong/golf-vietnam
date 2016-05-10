24c90fafcbfc6951b061cfedf94484ab48b4049d
thienlelong <thien.lelong.it@gmail.com>
2016-05-10 21:49:47 +0700

    finish payment page

--- wp-content/themes/nisarg/tmp-payment.php
+++ wp-content/themes/nisarg/tmp-payment.php
@@ -18,46 +18,83 @@
                 </div>
                 <article id="post-<?php the_ID(); ?>" <?php post_class('post-content'); ?>>
                     <div class="entry-content page-content">
-                        <div class="payment-info">
-                            <h2 class="titcontent">Items Details</h2>
-                            <div class="pmitem">
-                              <div class="pmorder row">
-                                  <div class="col-sm-6" ><h6 class="ordertit">Description</h6>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                  </div  >
-                                  <div class="col-sm-6 col-md-2" >
-                                    <h6 class="ordertit">QUANTITY</h6>
-                                    <input type="number" class="pmqty" min="0" step="1" max="9999" pattern="">
-                                  </div  >
-                                  <div class="col-sm-12 col-md-2" ><h6 class="ordertit">PRICE</h6>
-                                    <span>$49.95</span>
-                                  </div  >
-                                  <div  class="col-sm-12 col-md-2" ><h6 class="ordertit">TOTAL</h6>
-                                    <span>$72.45</span>
-                                  </div  >
-                              </div>
-                            </div>
-                        </div>
-                        <div class="payment-detail">
-                            <h2 class="titcontent">Payment Details</h2>
-                            <div class="pmitem">
-                                <div class="pmorder row">
-                                    <div class="col-sm-6" >
-                                        <div class="formgr"><label class="labelcon">Transaction Amount:</label>     <span class="formcon">$72.45 (CAD)</span></div>
-                                        <div class="formgr"><label class="labelcon">Order ID:</label>       <span class="formcon">mhp16104072730p58</span></div>
-                                    </div  >
-                                    <div class="col-sm-6 col-md-6" >
-                                         <img style="width: 100%;" src=<?php echo get_template_directory_uri()."/images/Paymentimgcard.png"?>>
-                                    </div  >
+                        <div class="inner-70">
+                            <div class="payment-info">
+                                <h2 class="page-title">Items Details</h2>
+                                <div class="pmitem">
+                                  <div class="pmorder row">
+                                      <div class="col-md-6">
+                                        <h4 class="pmitem-header">Description</h4>
+                                        <p class="pmitem-value text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                      </div>
+                                      <div class="col-xs-4 col-md-2" >
+                                        <h6 class="pmitem-header">QUANTITY</h6>
+                                        <input id="number-user" type="number" class="pmitem-value" min="0" step="1" max="9999" disabled="true">
+                                      </div  >
+                                      <div class="col-xs-4 col-md-2" ><h6 class="pmitem-header">PRICE</h6>
+                                        <span id="price" class="pmitem-value">$49.95</span>
+                                      </div  >
+                                      <div  class="col-xs-4 col-md-2" ><h6 class="pmitem-header">TOTAL</h6>
+                                        <span  id="total-price" class="pmitem-value">$72.45</span>
+                                      </div  >
+                                  </div>
                                 </div>
                             </div>
-                            <div class="col-sm-12" style="font-style:italic;margin: 20px 0">Please complete the following details exactly as they appear on your card. Do not put spaces or hyphens in the card number.</div>
+                            <div class="payment-detail">
+                                <h2 class="page-title">Payment Details</h2>
+                                <div class="pmitem">
+                                    <div class="row">
+                                        <div class="col-sm-6" >
+                                            <div class="pm-order">
+                                                <label>Transaction Amount:</label>
+                                                <span id="pm-total-price" class="cl-blue">$72.45 (CAD)</span>
+                                            </div>
+                                            <div class="pm-order">
+                                                <label>Order ID:</label>
+                                                <span class="cl-blue">mhp16104072730p58</span>
+                                                </div>
+                                        </div  >
+                                        <div class="col-sm-6" >
+                                             <img src=<?php echo get_template_directory_uri()."/images/Paymentimgcard.png"?>>
+                                        </div  >
+                                    </div>
+                                <p>Please complete the following details exactly as they appear on your card. Do not put spaces or hyphens in the card number.</p>
+                                </div>
+                            </div>
                         </div>
-                        <?php echo do_shortcode('[easy_payment item_name="Process Transaction" amount="9"]'); ?>
+                        <div class="payment-form row">
+
+                            <div class="row">
+                                <p class="col-sm-10 col-sm-offset-2 col-md-9 col-md-offset-3">By payment an account you agree to our <a href="<?php echo site_url('terms-and-conditions')?>">Terms and Conditions</a>  and our <a href="<?php echo site_url('privacy-policy')?>">Privacy Policy</a>.</p>
+                                <div class="col-sm-5 col-sm-offset-1 col-md-3 col-md-offset-3">
+                                    <button  id="btn-process-transaction" class="btn btn-radius btn-lg-13">process transaction</button>
+                                    <?php echo do_shortcode('[easy_payment amount="9"]'); ?>
+                                </div>
+                                <div class="col-sm-5 col-md-3"><a href="" class="btn btn-radius btn-lg-13 bg-red">cancel transaction</a></div>
+                            </div>
+                        </div>
+
+
                     </div><!-- .entry-content -->
                 </article><!-- #post-## -->
             <?php endwhile; // End of the loop. ?>
         </main><!-- #main -->
     </div><!-- #primary -->
 </div><!--.container-->
+<script type="text/javascript">
+    jQuery( document ).ready(function($) {
+        $('#number-user').val('2');
+        $('#price').html('$49.95');
+        $('#total-price').html('$72.45');
+        $('#pm-total-price').html('$72.45 (CAD)');
+
+        $('#easy_paypal_form_div form').attr('target', '');
+        $('#easy_paypal_form_div form input[name=amount]').val("72.45");
+        $('#easy_paypal_form_div form input[name=return]').val('http://golfvn.com/golf-vietnam/en/payment-success?uid=1');
+        $('#btn-process-transaction').click(function() {
+            console.log("haha");
+            $('#easy_paypal_form_div input[type=image]').trigger('click');
+        });
+    });
+</script>
 <?php get_footer(); ?>
